fd3c37bb848f847fa6f939165a50ba923a2cccb6
daboggg <daboggg@ya.ru>
2019-03-26 18:50:45 +0300

    end lesson 08

--- src/main/java/ru/zinin/sweater02/config/MvcConfig.java
+++ src/main/java/ru/zinin/sweater02/config/MvcConfig.java
@@ -1,14 +1,51 @@
 package ru.zinin.sweater02.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Value("${start.path}")
+    private String startPath;
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations(startPath + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/resources/templates/registration.ftl
+++ src/main/resources/templates/registration.ftl
@@ -2,11 +2,8 @@
 <#import "parts/login.ftl" as l>
 
 <@c.page>
-    Add new user
-    <#if message??>
-        ${message}
-    </#if>
-
-    <@l.login "/registration"/>
+    <div class="mb-1">Add new user</div>
+    ${message?ifExists}
+    <@l.login "/registration" true/>
 </@c.page>
 
